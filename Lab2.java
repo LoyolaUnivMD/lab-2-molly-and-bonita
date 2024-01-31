54e8f127e9a2a5d26e0ce68ac3feb94ea2f43823
mollykyle <mekyle@loyola.edu>
2024-01-31 17:52:46 -0500

    added to lab2

--- Lab2.java
+++ Lab2.java
@@ -9,17 +9,39 @@
 //            Reminder: you should never take code from the Internet or another person
 
 import java.util.Scanner;
+import java.util.Random;
+import java.text.DecimalFormat;
 
 class Lab2 {
     public static void main(String[] args) {
         long prize = 225938745L;
         Scanner input = new Scanner(System.in);
 
+        //Making object rand
+        Random rand = new Random();
+
+        //Making object decimal format
+        DecimalFormat dfmt = new DecimalFormat("##00");
+
         System.out.println("CS 212 - Lab 2");
         System.out.println("This program generates 10 lottery tickets.");
         System.out.print("What's your name? ");
         String customerName = input.nextLine();
 
+        String lottery = "";
+        int num = 0;
+        int numFormat = 0;
+        for(int i=0 ; i < 10 ; i++) {
+            for(int j=0 ; j < 6 ; j++) {
+
+                num = rand.nextInt(99);
+                DecimalFormat numFormat = dfmt.format(num);
+                System.out.println(num);
+//
+
+//                String lottery =
+            }
+        }
 
     }
 }
